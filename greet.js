0df7fa983531f0e06993989a0227194749ee47aa
Livia Mate <liviamate8@gmail.com>
2022-06-15 11:24:12 +0200

    A6

--- greet.js
+++ greet.js
@@ -1,5 +1,8 @@
-function greet() {
-    console.log("Greetings!");
+function greet(name) {
+    console.log(name);
   }
-  
-  greet("Lotti és Kitti");
+  let name1 = "Livi";
+  let name2 = "Kitti"
+  greet(name1);  
+  greet(name2);
+
